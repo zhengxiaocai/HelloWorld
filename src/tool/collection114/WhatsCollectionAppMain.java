d2455af41ee1d826b4b82b3ce383669218a2b83f
zhengxiaocai <zhengyuan_tester@163.com>
2020-06-07 22:49:43 +0800

    重新来一波

---
+++ src/tool/collection114/WhatsCollectionAppMain.java
@@ -0,0 +1,10 @@
+package tool.collection114;
+
+import java.util.Collection;
+
+public class WhatsCollectionAppMain {
+
+    public static void main(String[] args) {
+        Collection collection = null;
+    }
+}
