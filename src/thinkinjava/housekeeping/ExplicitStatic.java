e8df115d66080441aa11ab6af619233d66450e3d
zhengxiaocai <zhengyuan_tester@163.com>
2020-07-04 21:13:03 +0800

    类的初始化，静态成员初始化，成员变量初始化，静态初始化块，非静态初始化块。
    静态代码块》静态成员》main》代码块》成员变量》构造器

---
+++ src/thinkinjava/housekeeping/ExplicitStatic.java
@@ -0,0 +1,37 @@
+package thinkinjava.housekeeping;
+
+class Cup {
+    Cup(int marker) {
+        System.out.println("Cup(" + marker + ")");
+    }
+
+    void f(int marker) {
+        System.out.println("f(" + marker + ")");
+    }
+}
+
+class Cups{
+    static Cup cup1;
+    static Cup cup2;
+
+    static {
+        cup1 = new Cup(1);
+        cup2 = new Cup(2);
+    }
+
+    Cups(){
+        System.out.println("Cups");
+    }
+}
+
+public class ExplicitStatic {
+
+    public static void main(String[] args) {
+        System.out.println("Inside main");
+//        Cups.cup1.f(99);
+    }
+
+    //>>TODO: 这个在main方法之前。
+    static Cups cups1 = new Cups();
+    static Cups cups2 = new Cups();
+}
---
+++ src/thinkinjava/housekeeping/Spoon.java
@@ -0,0 +1,12 @@
+package thinkinjava.housekeeping;
+
+public class Spoon {
+
+    //>>TODO: 可以将一组静态初始化动作放到类里面一个特殊的 静态代码块中
+
+    static int i;
+
+    static {
+        i = 47;
+    }
+}
