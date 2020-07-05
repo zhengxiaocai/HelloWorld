f6b946a328332d2272a043ce7f493717092179c2
zhengxiaocai <zhengyuan_tester@163.com>
2020-07-05 22:06:22 +0800

    可变长度的参数

---
+++ src/algorithm/Notation01.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class Notation01 {
+
+    public static void main(String[] args) {
+
+        // 时间复杂度为 O(1)
+        int n = 1000;
+        System.out.println("Hey - your input is : " + n);
+
+        // 时间复杂度为 O(1)
+        int m = 1000;
+        System.out.println("Hey - your input is : " + m);
+        System.out.println("Hmm.. I'm doing more stuff with : " + m);
+        System.out.println("And more: " + m);
+    }
+}
